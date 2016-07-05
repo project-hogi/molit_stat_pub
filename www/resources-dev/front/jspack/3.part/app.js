6c527d732c6ef97a821e5aaa64f9b4cbce3b1bee
수빈 <sbeeeeee@naver.com>
2016-07-05 13:54:37 +0900

    zzz

--- www/resources-dev/front/jspack/3.part/app.js
+++ www/resources-dev/front/jspack/3.part/app.js
@@ -8,9 +8,64 @@ jQuery(function($) {
 	$('ul.db-list li dl dt').on('click', function() {
 		$(this).parent('dl').find('dd').toggle();
 	});
-	
+
 	$('.data-year-view .ri ul li dl dt').on('click', function() {
 		$(this).parent('dl').find('dd').toggle();
 	});
-	
+
+	$('.lnb').each(function() {
+		var $lnb = $(this);
+		var $statNav = $(this).find('>ul>li.stat');
+		var $statDepth2Wrap = $('.stat-depth2');
+		var timeoutId = null;
+
+		$statNav.on('mouseenter', function() {
+			$lnb.trigger('start');
+		});
+		$(this).find('>ul>li').not($statNav).on('mouseenter', function() {
+			$lnb.trigger('endNow');
+		});
+		$statNav.on('mouseleave', function() {
+			$lnb.trigger('end');
+		});
+		$statDepth2Wrap.find('>*').on('mouseenter', function() {
+			$lnb.trigger('start');
+		});
+		$statDepth2Wrap.find('>*').on('mouseleave', function() {
+			$lnb.trigger('end');
+		});
+		$statDepth2Wrap.find('.close a').on('click', function(event) {
+			event.preventDefault();
+			
+			$lnb.trigger('endNow');
+		});
+		$(this).on('start', function() {
+			$statDepth2Wrap.fadeIn();
+
+			timeoutId != null && clearTimeout(timeoutId);
+		});
+		$(this).on('end', function() {
+			timeoutId != null && clearTimeout(timeoutId);
+
+			timeoutId = setTimeout(function() {
+				$statDepth2Wrap.stop().fadeOut();
+			}, 500);
+		});
+		$(this).on('endNow', function() {
+			timeoutId != null && clearTimeout(timeoutId);
+
+			$statDepth2Wrap.stop().fadeOut();
+		});
+	});
+
+	$(window).scroll(function() {
+
+		if ($(window).scrollTop() >= 128) {
+			$('.snb').addClass('fixed');
+		} else {
+			$('.snb').removeClass('fixed');
+		}
+
+	});
+
 });
--- www/views/part/header.php
+++ www/views/part/header.php
@@ -27,7 +27,7 @@
 			<div class="lnb">
 				<ul>
 					<li><a href="notice-use.php">알림마당</a></li>
-					<li><a href="stat-field.php">통계마당</a></li>
+					<li class="stat"><a href="stat-field.php">통계마당</a></li>
 					<li><a href="data-year.php">자료마당</a></li>
 					<li><a href="community-report.php">참여마당</a></li>
 				</ul>
@@ -40,6 +40,36 @@
 				<input type="button" value="검색" />
 			</div>
 		</div>
+	</div>
+	<div class="stat-depth2" style="display:none;">
+		<p class="img">
+			<img src="<?php echo $config["imageFolder"]; ?>/common/stat-depth2.png" alt="" />
+			<div id="container">
+				<ul class="menu">
+					<li>
+						<a href="stat-field.php">분야별통계</a>
+					</li>
+					<li>
+						<a href="stat-department.php">부서별통계</a>
+					</li>
+					<li>
+						<a href="stat-name.php">명칭별통계</a>
+					</li>
+					<li>
+						<a href="stat-area.php">지역별통계</a>
+					</li>
+					<li>
+						<a href="stat-field.php">영문통계</a>
+					</li>
+					<li>
+						<a href="stat-other.php">타기관통계</a>
+					</li>
+				</ul>
+				<p class="close">
+					<a href="#">close</a>
+				</p>
+			</div>
+		</p>
 	</div>
 	<div class="body">
 		<div id="container">
